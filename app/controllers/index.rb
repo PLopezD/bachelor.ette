876d9592bfe6aa169912ca1e426c3158315a9fe5
Thomas Abend & Robert Yang <pair+tsabend.unknownbreaker@devbootcamp.com>
2014-10-10 15:52:58 -0400

    login page complete

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,12 +1,45 @@
+# LOG IN PAGE
+
 get '/' do
   erb :index
 end
 
-get '/signup' do
+post '/' do
+  user = User.find_by(email: params[:email])
+  if user
+    if User.authenticate(params[:email], params[:password])
+      session[:user_id] = user.id
+    else
+    session[:errors] = {error: ["Invalid Password."]}
+    end
+  else
+    session[:errors] = {error: ["Username not found. Create an account."]}
+  end
+  redirect '/'
+end
 
+
+# SIGN UP PAGE
+get '/signup' do
   erb :signup
 end
 
 post '/signup' do
-
+  user = User.new(params[:args])
+  password = params[:args][:password_hash]
+# Feel free to add more password validations below
+  if password.length < 6
+    session[:errors] = {error: ["Password must be at least 6 characters."]}
+    redirect "/signup"
+  end
+  if user.valid?
+    user.password = password
+    user.save!
+    session[:user_id] = user.id
+    redirect ('/')
+  else
+    errors = user.errors.messages
+    session[:errors] = errors
+    redirect "/signup"
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,28 @@
 class User < ActiveRecord::Base
   # Remember to create a migration!
-  validates :email, :uniqueness => true
-
+  validates :email, uniqueness: true,
+    format: { with: /\S{1,}@\S{1,}.\S{2,}/,
+    message: "Email must be properly formated and unique."}
   has_many  :future_bachs
   has_many  :hookups
   has_many  :proposals
+
+
+  include BCrypt
+
+  def self.authenticate(email, given_password)
+    user = User.find_by(email: email)
+    user.password == given_password ? user : nil
+  end
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+
 end
